68825a2e1e0ab6021321870e44da117d526e691b
mukuu <kentaro.okano@gmail.com>
2011-11-15 17:08:29 +0900

    test

---
+++ rubyテスト/test.rb
@@ -0,0 +1,19 @@
+class Test
+  print "Hello world\n"
+
+  #!/bin/env ruby
+  # require 'rubygems'
+  # require 'databasedotcom'
+
+  # クライアントインスタンスの作成
+=begin
+  client = Databasedotcom::Client.new :client_id => "3MVG9I1kFE5Iul2AcMJ8XA_UH94NcnxdNlbvhyPEQm4TB2LKupCIApn33vumX1qU5NEmjf0aQq.fNtM3onH0h", :client_secret => "5878293312213036622"
+  username = 'kentaro.okano@gmail.com'
+  password = 'crescent110'
+  security_token = 'dhHH2eDt6PUOJF242Oh08Wvgh'
+=end
+  # 認証
+  # client.authenticate(:username =>username, :password =>password + security_token)
+
+  # p client.list_sobjects
+end
